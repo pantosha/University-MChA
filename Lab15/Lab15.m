5c99c3927880a17f4e29a8e415b5a5fd1e006753
Paulovich Anton <p.a.n.tosha@gmail.com>
2014-12-15 00:41:45 +0300

    Update Lab15.

--- Lab15/Lab15.m
+++ Lab15/Lab15.m
@@ -7,7 +7,7 @@
 % du/dt = a*d^2u/dx^2 + a*d^2u/dy^2, a > 0
 % u(0, y, t) = sinh(y)*exp(-3*a*t)
 % u(pi/2, y, t) = -sinh(y)*exp(-3*a*t)
-% u(x, 0, t) = cos(2x)*exp(-3*a*t)
+% u(x, 0, t) = 0
 % u(x, ln(2), t) = 3/4 * cos(2*x)*exp(-3*a*t),
 % u(x, y, 0) = cos(2*x)*sinh(y)
 
@@ -15,5 +15,20 @@
 %   U(x, y, t) = cos(2*x)*sinh(y)*exp(-3*a*t)
 
 % —хема переменных направлений
+uc = zeros(length(x), length(y), length(t));
+for j = 1:length(x)
+    for k = 1:length(y)
+        uc(j, k, :) = cos(2.*x(j)).*sinh(y(k)).*exp(-3.*s.*t);
+    end
+end
 
-
+% su = surf(y, x, uc(:,:,1));
+% set(su, 'LineStyle', 'none');
+% while 1
+%     for i = 1:2:length(t);
+%         su = surf(y, x, u(:,:,i));
+%         set(su, 'LineStyle', 'none');
+%         zlim([-0.5 0.5]);
+%         f = getframe;
+%     end;
+% end;
--- Lab15/Lab15_1.m
+++ Lab15/Lab15_1.m
@@ -1,36 +1,82 @@
 % Схема дробных шагов
+s = 1;
+f = @(x, y, t)0;
+gx_l = @(y, t)sinh(y).*exp(-3.*s.*t);
+gx_r = @(y, t)-sinh(y).*exp(-3.*s.*t);
+gy_l = @(x, t)0;
+gy_r = @(x, t)0.75.*cos(2.*x).*exp(-3.*s.*t);
 
-% u(x, y, t)
-% f(x, y, t)
+hx = 0.05;
+hy = 0.01;
+tao = 0.01;
 
-% n - номер точки по t
-% j - номер точки по x
-% k - номер точки по y
+x = 0 : hx : pi/2;
+y = 0 : hy : log(2);
+t = 0 : tao : 2;
 
-% Неявная разностная схема
-j = 2:length(x)-1;
-alpha = zeros(size(x));
-beta = zeros(size(x));
-gamma = zeros(size(x));
-for i = 1:length(t)-1
-    alpha(1) = 1;
-    beta(1) = -h*g_1(t(i+1));
-    gamma(j) = u(j, k, n) + tao.*f(x(j), y(k), t(i));
-    
-    a = -s*tao/hx^2;
-    b = 1 + 2*s*tao/hx^2 + k*tao;
-    c = -s*tao/hx^2;
-    
-    % Прямой ход
-    for nj = j
-        alpha(nj) = -a/(b + c*alpha(nj-1));
-        beta(nj) = (gamma(nj) - c*beta(nj-1))/(b + c*alpha(nj-1));
+% Начальные и граничные условия
+u = zeros(length(x), length(y), 2*length(t));
+for j = 1:length(x)
+    u(j, :, 1) = cos(2*x(j)).*sinh(y);
+end
+for n = 1:length(t)
+    u(1, :, n) = gx_l(y, n);
+    u(end, :, n) = gx_r(y, n);
+    u(:, 1, n) = gy_l(x, n);
+    u(:, end, n) = gy_r(x, n);
+end
+
+for n = 1:2:length(t)-2
+    alpha = zeros(length(x));
+    beta = zeros(length(x));
+    for k = 2:length(y)-1
+        alpha(1) = 0;
+        beta(1) = gx_l(y(k-1), t(n+1));
+        a = -s*tao/hx^2;
+        b = 1 + 2*s*tao/hx^2;% + k.*tao;
+        c = -s*tao/hx^2;
+        % Прямой ход
+        for j = 2:length(x)-1
+            gamma = u(j, k, n) + tao*f(x(j), y(k), t(n));
+            alpha(j) = -a/(b + c*alpha(j-1));
+            beta(j) = (gamma - c*beta(j-1))/(b + c*alpha(j-1));
+        end
+        u(end, k, n+1) = gx_r(y(k), t(n+1));
+        % Обратный ход
+        for j = length(x)-1:-1:1
+            u(j, k, n+1) = alpha(j).*u(j+1, k, n+1) + beta(j);
+        end
     end
-    
-    u(i+1, end) = (h*g_2(t(i+1)) + beta(end-1))/(1-alpha(end-1));
-    
-    % Обратный ход
-    for nj = length(x)-1:-1:1
-        u(i+1, nj) = alpha(nj)*u(i+1, nj+1) + beta(nj);
+    alpha = zeros(length(y));
+    beta = zeros(length(y));
+    for j = 2:length(x)-1
+        alpha(1) = 0;
+        beta(1) = gy_l(x(j-1), t(n+2));
+        a = -s*tao/hy^2;
+        b = 1 + 2*s*tao/hy^2;
+        c = -s*tao/hy^2;
+        % Прямой ход
+        for k = 2:length(y)-1
+            gamma = u(j, k, n+1);
+            alpha(k) = -a./(b + c.*alpha(k-1));
+            beta(k) = (gamma - c.*beta(k-1))./(b + c.*alpha(k-1));
+        end
+        u(j, end, n+2) = gy_r(x(j), t(n+2));
+        % Обратный ход
+        for k = length(y)-1:-1:1
+            u(j, k, n+2) = alpha(k).*u(j, k+1, n+2) + beta(k);
+        end
     end
 end
+
+%% Анимация
+% su = surf(y, x, u(:,:,5));
+% set(su, 'LineStyle', 'none');
+while 1
+    for i = 1:2:length(t);
+        j = surf(y, x(2:end-1), u(2:end-1, :, i));
+        set(j, 'LineStyle', 'none');
+        zlim([-0.5 0.5]);
+        f = getframe;
+    end;
+end;
